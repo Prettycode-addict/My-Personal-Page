e3a9aa563229cbe0a283a25fa7ac11830f35a11f
Abdul Malik <malikbanks01@gmail.com>
2018-05-24 14:38:06 +0000

    Included js scripts

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+ $(function(){
+      prompt("Hey buddy, enter your Name let's get rocking - ");
+   });
+
+function myFunction() {
+    var txt;
+    var person = prompt("Please enter your name:", "Harry Potter");
+    if (person == null || person == "") {
+        txt = "User cancelled the prompt.";
+    } else {
+        txt = "Hello " + person + "! How are you today?";
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
